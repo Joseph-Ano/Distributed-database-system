11e12063b766fe1ff0c55f7e115ec4bb948774e9
Joseph-Ano <80886311+Joseph-Ano@users.noreply.github.com>
2023-03-28 19:17:18 +0800

    connected to db

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const dbService = require('./dbService');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ server/dbService.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+})
+
+connection.connect((err) => {
+    if(err){
+        console.log(err.message);
+    }
+    
+    console.log('db is connected');
+})
